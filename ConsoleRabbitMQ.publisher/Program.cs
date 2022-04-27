71a8a594f5dfd81cf07e3efdcf7a1532fad18680
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-04-27 05:01:34 +0300

    Complex data örneği

--- ConsoleRabbitMQ.publisher/Program.cs
+++ ConsoleRabbitMQ.publisher/Program.cs
@@ -1,8 +1,10 @@
 ﻿using RabbitMQ.Client;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 
 namespace ConsoleRabbitMQ.publisher
 {
@@ -19,17 +21,27 @@ namespace ConsoleRabbitMQ.publisher
 
             channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
 
-            Dictionary<string,object> headers = new Dictionary<string, object>();
+            Dictionary<string, object> headers = new Dictionary<string, object>();
 
-            headers.Add("format","pdf");
+            headers.Add("format", "pdf");
             //headers.Add("shape","a4");
-            headers.Add("shape2","a4");
+            headers.Add("shape2", "a4");
 
             var properties = channel.CreateBasicProperties();
             properties.Headers = headers;
             properties.Persistent = true;
 
-            channel.BasicPublish("header-exchange",string.Empty, properties,Encoding.UTF8.GetBytes("header mesajım"));
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Kalem",
+                Price = 100,
+                Stock = 10
+            };
+
+            var productJsonString = JsonSerializer.Serialize(product);
+
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
 
             Console.WriteLine("mesaj gönderilmiştir.");
 
--- ConsoleRabbitMQ.subscriber/Program.cs
+++ ConsoleRabbitMQ.subscriber/Program.cs
@@ -1,9 +1,11 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 
 namespace ConsoleRabbitMQ.subscriber
@@ -42,9 +44,11 @@ namespace ConsoleRabbitMQ.subscriber
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
+                Product product = JsonSerializer.Deserialize<Product>(message);
+
                 Thread.Sleep(1500);
 
-                Console.WriteLine("Gelen mesaj :" + message);
+                Console.WriteLine($"Gelen mesaj : {product.Id}-{product.Name}-{product.Price}-{product.Stock}");
 
                 channel.BasicAck(e.DeliveryTag, false);
             };
